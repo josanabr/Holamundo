818f6ec0ca9a15e9eecec71de615fe481574c937
John Sanabria <johnsanabria@correounivalle.edu.co>
2020-04-12 17:44:10 -0500

    Primer commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Primer programa en C
+
+Este repositorio le permitirá a usted demostrar sus habilidades con el lenguaje C. 
+Por favor complete el programa en este repositorio de acuerdo a las instrucciones que se dan dentro del programa en .C, [`holamundo.c`](holamundo.c).
---
+++ holamundo.c
@@ -0,0 +1,14 @@
+/**
+  Incluya los archivos de cabecera que definen el prototipo de la funcion 'printf'
+*/
+
+/**
+  Escriba lo que devuelve la funcion 'main' y los parametros que esta recibe.
+*/
+main() {
+/**
+  Escriba las instrucciones que permitan imprimir por pantalla la frase 'Hola mundo'
+*/
+
+  return 0;
+}
